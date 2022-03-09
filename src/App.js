c3eebffc44c4909f34cb4a91cad3e0c08157ff6c
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-09 18:45:56 +0800

    change folder location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import NotFound from './components/pages/Notfound'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/Notfound'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
