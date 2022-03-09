a2f9212dc039a2c800ec41416902e964554f8cba
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-09 18:40:21 +0800

    init pages / routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/Notfound'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
@@ -9,7 +12,11 @@ function App() {
         <Navbar />
 
         <main className='container mx-auto px-3 pb-12'>
-          content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
         </main>
 
         <Footer />
