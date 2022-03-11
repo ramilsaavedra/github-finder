9d09654e26b8fee78a96ae174bd6f3f708a89ba1
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-11 10:19:55 +0800

    add action setloading

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        isLoading: true
+        isLoading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async() => {
+        setLoading()
+        
         const response = await fetch(`${GITHUB_URL}/users`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
@@ -29,6 +31,9 @@ export const GithubProvider = ({children}) => {
         })
 
     }
+
+    //set loading
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
         users: state.users,
