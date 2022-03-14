dc4c31c02d3840bdfd0c2b19396f035c91307038
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-14 17:21:14 +0800

    repoitem component

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import RepoItem from './RepoItem'
 import PropTypes from 'prop-types'
 
 function RepoList({repos}) {
@@ -6,8 +7,8 @@ function RepoList({repos}) {
     <div className='rounded-lg shadow-lg card bg-base-100'>
         <div className="card-body">
             <h2 className='text-3xl my-4 font-bold card-title'> Latest Repositories</h2>
-            {repos.map((repo, index) => (
-                <h3 key={index}>{repo.name}</h3>
+            {repos.map((repo) => (
+                <RepoItem key={repo.id} repo={repo} />
             ))}
         </div>
     </div>
